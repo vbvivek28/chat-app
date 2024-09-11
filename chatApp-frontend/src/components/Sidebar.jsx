6032c65eece59b306d0d5dd7becd5a20b9a6e185
vbvivek28 <vb79798@gmail.com>
2024-09-11 12:21:31 +0530

    minor changes

--- chatApp-frontend/src/components/Sidebar.jsx
+++ chatApp-frontend/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ function Sidebar() {
   return (
     <div className='w-full h-full'>
         <div className='bg-slate-200 w-12 h-full rounded-tr-lg rounded-br-lg py-5'>
-           <div className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200' title='chat'>
+           <div className='w-12 h-121 flex justify-center items-center cursor-pointer hover:bg-slate-200' title='chat'>
             <IoChatbubbleEllipses 
             size={25} />
             </div> 
