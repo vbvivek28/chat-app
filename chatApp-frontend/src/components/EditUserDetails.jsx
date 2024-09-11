9de3debe050eb4e0ed42298014f1a3e9fca9da67
vivek bisht <vb79798@gmail.com>
2024-09-11 14:08:42 +0530

    edit user detail

---
+++ chatApp-frontend/src/components/EditUserDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditUserDetails=({onClose,data})=>{
+    return(
+        <div className="fixed top-0 bottom-0 left-0 right-0 bg-gray-700 bg-opacity-40 flex justify-center items-center">
+            <div className="bg-white p-4 m-1 rounded w-full max-w-sm ">
+            edit details</div>
+        </div>
+    )
+}
+export default EditUserDetails;
--- chatApp-frontend/src/components/Sidebar.jsx
+++ chatApp-frontend/src/components/Sidebar.jsx
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoChatbubbleEllipses } from 'react-icons/io5'
+import { FaUserPlus } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import { NavLink } from 'react-router-dom'
+import Avatar from './Avatar.jsx';
+import { useSelector } from 'react-redux';
+import userSlice from '../redux/userSlice';
+import EditUserDetails from './EditUserDetails.jsx';
 function Sidebar() {
+  const user=useSelector(state=> state?.user)
+  const [ editUserOpen, setEditUSerOpen]=useState(false)
   return (
     <div className='w-full h-full'>
-        <div className='bg-slate-200 w-12 h-full rounded-tr-lg rounded-br-lg py-5'>
-           <div className='w-12 h-121 flex justify-center items-center cursor-pointer hover:bg-slate-200' title='chat'>
+        <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between' >
+         <div>
+         <NavLink className={({isActive})=>`w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200 rounded ${isActive && 'bg-slate-200'}`} title='chat'>
             <IoChatbubbleEllipses 
             size={25} />
+            </NavLink> 
+
+            <div className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200' title='chat'>
+            <FaUserPlus 
+            size={25} />
             </div> 
+         </div>
+
+         <div className='flex flex-col items-center'>
+          <button className='mx-auto' title={user?.name }  onClick={()=>{setEditUSerOpen(true)}}>
+            <Avatar width={40} height={40}
+            name={user?.name}/>
+          </button>
+         <button title="logout" className='w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-200' >
+            <span className='-ml-1'><BiLogOut 
+            size={25} /></span>
+            </button>           
+         </div>
         </div>
+
+        {editUserOpen && (<EditUserDetails onClose={()=>setEditUSerOpen(false)} data={user}/>)}
     </div>
   )
 }
